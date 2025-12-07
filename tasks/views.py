da7a76117251991529ad7141066e3e33b0689f03
Rabbadhaki <badha.ron09@gmail.com>
2025-12-07 14:35:53 +0300

    Create views.py

---
+++ tasks/views.py
@@ -0,0 +1,22 @@
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user).order_by('-created_at')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    @action(detail=True, methods=['post'])
+    def complete(self, request, pk=None):
+        task = self.get_object()
+        task.completed = True
+        task.save()
+        return Response(self.get_serializer(task).data, status=status.HTTP_200_OK)

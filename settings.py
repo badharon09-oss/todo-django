d714d3394c2a3af9a3da70aa3a2ea1dec5f3c4bc
Rabbadhaki <badha.ron09@gmail.com>
2025-12-07 14:33:13 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,16 @@
+INSTALLED_APPS = [
+    # default Django apps...
+    'rest_framework',
+    'users',   # our users app (optional; using built-in User)
+    'tasks',
+]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',  # if using JWT
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
 
